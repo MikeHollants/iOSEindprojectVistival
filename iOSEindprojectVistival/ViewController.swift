429364909ae9a6c03c9ea861ce1f3e006bb9aa8d
Michael Hollants <m.hollants@gmail.com>
2018-01-29 11:54:50 +0100

    Initial Commit

---
+++ iOSEindprojectVistival/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSEindprojectVistival
+//
+//  Created by Michael Hollants on 29/01/2018.
+//  Copyright © 2018 ehb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
