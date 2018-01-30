93c95cee6b418e4454f26d0efd42a86925015c05
David Welleman <dajian@MacBook-Pro-de-Dajian.local>
2018-01-30 09:35:34 +0100

    update icons added background image

--- iOSEindprojectVistival/LineUpViewController.swift
+++ iOSEindprojectVistival/LineUpViewController.swift
@@ -12,8 +12,8 @@ class LineUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //Set a background image in place of the color
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
     }
 
     override func didReceiveMemoryWarning() {
