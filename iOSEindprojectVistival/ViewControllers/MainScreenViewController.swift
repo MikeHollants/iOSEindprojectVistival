7fb67e39244a5ebfea78f90b69d60baf07c31dfe
Michael Hollants <m.hollants@gmail.com>
2018-01-30 13:19:15 +0100

    Commit incl MapViewController code

--- iOSEindprojectVistival/ViewControllers/MainScreenViewController.swift
+++ iOSEindprojectVistival/ViewControllers/MainScreenViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainScreenViewController: UIViewController {
+class MainScreenViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
