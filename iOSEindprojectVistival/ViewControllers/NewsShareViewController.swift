875579abee24a5e36bc8ba458000920dfb0498f0
David Welleman <dajian@mbp-de-dajian.lan>
2018-01-31 18:44:39 +0100

    news full list done change lait content operational link to news details done
    to do
    
    update news details screen graphics for lisibility
    pop up to connect social media and react

--- iOSEindprojectVistival/ViewControllers/NewsShareViewController.swift
+++ iOSEindprojectVistival/ViewControllers/NewsShareViewController.swift
@@ -9,8 +9,17 @@
 import UIKit
 
 class NewsShareViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+
     
-    var postList:[Post] = [Post]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    var facebookList:[Post] = [Post]()
+    var instagramList:[Post] = [Post]()
+    var tweeterList:[Post] = [Post]()
+    var fullList:[Post] = [Post]()
+    var currentList:[Post] = [Post]()
+    var channel:Int = 3
+    @IBOutlet weak var lbChannelName: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,41 +28,42 @@ class NewsShareViewController: UIViewController , UITableViewDelegate , UITableV
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
         
-        var postTxt:String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque turpis luctus, blandit felis vitae, vulputate est. Nam sagittis, erat a interdum porttitor, sapien justo suscipit urna, sed semper lorem mi ut velit. Donec euismod massa interdum semper porttitor. Phasellus maximus auctor lorem quis porta. Morbi sit amet molestie purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc efficitur dui id purus tristique, ac pharetra tortor vehicula. Sed id nunc ornare, ultricies nunc nec, scelerisque velit."
+        let postTxt:String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque turpis luctus, blandit felis vitae, vulputate est. Nam sagittis, erat a interdum porttitor, sapien justo suscipit urna, sed semper lorem mi ut velit. Donec euismod massa interdum semper porttitor. Phasellus maximus auctor lorem quis porta. Morbi sit amet molestie purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc efficitur dui id purus tristique, ac pharetra tortor vehicula. Sed id nunc ornare, ultricies nunc nec, scelerisque velit."
         
-        var datePost:Date = Date.init()
+        let datePost:Date = Date.init()
         
-        var postPic1:UIImage = UIImage.init(named: "post2.jpg")!
-        var postPic2:UIImage = UIImage.init(named: "post1.jpg")!
-        var postPic3:UIImage = UIImage.init(named: "post3.jpg")!
+        let postPic1:UIImage = UIImage.init(named: "post2.jpg")!
+        let postPic2:UIImage = UIImage.init(named: "post1.jpg")!
+        let postPic3:UIImage = UIImage.init(named: "post3.jpg")!
         
-        var post1:Post = Post.init(title: "Color Mix at the VisTivaL !!", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
+        let post1:Post = Post.init(title: "Facebook 1", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
+        let post2:Post = Post.init(title: "Facebook 2", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post3:Post = Post.init(title: "Facebook 3", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post4:Post = Post.init(title: "Facebook 4", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
+        let post5:Post = Post.init(title: "Facebook 5", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post6:Post = Post.init(title: "Facebook 6", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post7:Post = Post.init(title: "Facebook 7!", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
+        let post8:Post = Post.init(title: "Tweeter 1", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post9:Post = Post.init(title: "Tweeter 2", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post10:Post = Post.init(title: "Tweeter 3", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
+        let post11:Post = Post.init(title: "Tweeter 4", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post12:Post = Post.init(title: "Tweeter 5", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post13:Post = Post.init(title: "Tweeter 6", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post14:Post = Post.init(title: "Tweeter 7", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post15:Post = Post.init(title: "Instagram 1", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post16:Post = Post.init(title: "Instagram 2", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post17:Post = Post.init(title: "Instagram 3", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
+        let post18:Post = Post.init(title: "Instagram 4", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post19:Post = Post.init(title: "Instagram 5", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
+        let post20:Post = Post.init(title: "Instagram 6", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        let post21:Post = Post.init(title: "Instagram 7", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
         
-        var post2:Post = Post.init(title: "Enjoy Vistival", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
+        facebookList += [post1,post2,post3,post4,post5,post6,post7]
+        tweeterList += [post8,post9,post10,post11,post12,post13,post14]
+        instagramList += [post15,post16,post17,post18,post19,post20,post21]
         
-        var post3:Post = Post.init(title: "See Music & Shrimps LOL", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
-        
-        var post4:Post = Post.init(title: "Color Mix at the VisTivaL !!", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
-        
-        var post5:Post = Post.init(title: "Enjoy Vistival", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
-        
-        var post6:Post = Post.init(title: "See Music & Shrimps LOL", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
-        
-        var post7:Post = Post.init(title: "Color Mix at the VisTivaL !!", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
-        
-        var post8:Post = Post.init(title: "Enjoy Vistival", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
-        
-        var post9:Post = Post.init(title: "See Music & Shrimps LOL", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
-        
-        var post10:Post = Post.init(title: "Color Mix at the VisTivaL !!", author: "Dajian", content: postTxt, date: datePost, image: postPic1)
-        
-        var post11:Post = Post.init(title: "Enjoy Vistival", author: "Dajian", content: postTxt, date: datePost, image: postPic2)
-        
-        var post12:Post = Post.init(title: "See Music & Shrimps LOL", author: "Dajian", content: postTxt, date: datePost, image: postPic3)
-        
-        postList += [post1,post2,post3,post4,post5,post6,post7,post8,post9,post10,post11,post12]
-        
-        
+        fullList += [post1,post2,post3,post4,post5,post6,post7,post8,post9,post10,post11,post12,post13,post14,post15,post16,post17,post18,post19,post20,post21]
+        currentList = fullList
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,63 +71,103 @@ class NewsShareViewController: UIViewController , UITableViewDelegate , UITableV
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    @IBAction func selChannel(_ sender: UIButton) {
+        
+        channel = sender.tag
+        
+        switch channel {
+            
+        case 0:
+            lbChannelName.text = "Tweeter Channel"
+            currentList = tweeterList
+            tableView.reloadData()
+        case 1:
+            lbChannelName.text = "Instagram Channel"
+            currentList = instagramList
+            tableView.reloadData()
+        case 2:
+            lbChannelName.text = "Facebook Channel"
+            currentList = facebookList
+            tableView.reloadData()
+        default:
+            lbChannelName.text = "Vistival Channel"
+            currentList = fullList
+    }
+    
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postList.count
+        return currentList.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        tableView.backgroundColor = .clear
-        
-        if indexPath.row == 0 {
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
-            let cell:TopPostTableViewCell = tableView.dequeueReusableCell(withIdentifier: "TopPostCell", for: indexPath) as! TopPostTableViewCell
-            
-            let TopPost = postList[indexPath.row]
-            let formater = DateFormatter()
-            formater.dateStyle = .short
-            formater.timeStyle = .short
-            let date = TopPost.date
-            
-            cell.ivTopPost.image = TopPost.image
-            cell.lbTitleTopPost.text = TopPost.title
-            cell.lbDateTopPost.text = formater.string(from: date)
-            cell.lbAuthor.text = TopPost.author
-            
-            return cell
-            
-        }else{
-            
-            let cell:PostTableViewCell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
-            
-            let Post = postList[indexPath.row]
-            let formater = DateFormatter()
-            formater.dateStyle = .short
-            formater.timeStyle = .short
-            let date = Post.date
-            
-            cell.ivPost.image = Post.image
-            cell.lbTitlePost.text = Post.title
-            cell.lbDatePost.text = formater.string(from: date)
-            
-            return cell
-            
-        }
-        
-        
-        
+            if indexPath.row == 0 {
+                
+                let cell:TopPostTableViewCell = tableView.dequeueReusableCell(withIdentifier: "TopPostCell", for: indexPath) as! TopPostTableViewCell
+                
+                let TopPost = currentList[indexPath.row]
+                let formater = DateFormatter()
+                formater.dateStyle = .short
+                formater.timeStyle = .short
+                let date = TopPost.date
+                
+                cell.ivTopPost.image = TopPost.image
+                cell.lbTitleTopPost.text = TopPost.title
+                cell.lbDateTopPost.text = formater.string(from: date)
+                cell.lbAuthor.text = TopPost.author
+                
+                return cell as UITableViewCell
+                
+            }else{
+                
+                let cell:PostTableViewCell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+                
+                let Post = currentList[indexPath.row]
+                let formater = DateFormatter()
+                formater.dateStyle = .short
+                formater.timeStyle = .short
+                let date = Post.date
+                
+                cell.ivPost.image = Post.image
+                cell.lbTitlePost.text = Post.title
+                cell.lbDatePost.text = formater.string(from: date)
+                
+                return cell as UITableViewCell
+                
+            }
     }
     
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+        let destination:PostDetailsViewController = segue.destination as! PostDetailsViewController
+        
+        if let cell:TopPostTableViewCell = sender as? TopPostTableViewCell {
+            
+            var index = self.tableView.indexPath(for: cell)
+            destination.Post = currentList[(index?.row)!]
+            
+        }else if let cell:PostTableViewCell = sender as? PostTableViewCell {
+            
+            var index = self.tableView.indexPath(for: cell)
+            destination.Post = currentList[(index?.row)!]
+        }
+        
     }
-    */
+
 
 }
---
+++ iOSEindprojectVistival/ViewControllers/PostDetailsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PostDetailsViewController.swift
+//  iOSEindprojectVistival
+//
+//  Created by Dajian on 31/01/18.
+//  Copyright © 2018 ehb. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var lbTitlePost: UILabel!
+    @IBOutlet weak var lbDatePost: UILabel!
+    @IBOutlet weak var ivImagePost: UIImageView!
+    @IBOutlet weak var tvContentPost: UITextView!
+    @IBOutlet weak var lbAuthor: UILabel!
+    
+    var Post:Post!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Set a background image in place of the color
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        
+        let formater = DateFormatter()
+        formater.dateStyle = .short
+        formater.timeStyle = .short
+        let date = Post.date
+        
+        ivImagePost.image = Post.image
+        lbTitlePost.text = Post.title
+        lbDatePost.text = formater.string(from: date)
+        tvContentPost.text = Post.content
+        lbAuthor.text = Post.author
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
