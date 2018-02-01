509e8ebfb5a9b817a1695db92243ae51086e0c79
Michael Hollants <m.hollants@gmail.com>
2018-02-01 21:02:45 +0100

    MapVC pin personalised?

--- iOSEindprojectVistival/ViewControllers/MapViewController.swift
+++ iOSEindprojectVistival/ViewControllers/MapViewController.swift
@@ -17,8 +17,9 @@ class MapPoint: NSObject, MKAnnotation {
     
     var coordinate: CLLocationCoordinate2D
     var title: String?
+    let pinImage = UIImage(named: "Annotation-pic.png")
     
-    init(coordinate: CLLocationCoordinate2D, title: String) {
+    init(coordinate: CLLocationCoordinate2D, title: String, pinImage: UIImage) {
         self.coordinate = coordinate
         self.title = title
         }
@@ -36,37 +37,57 @@ class MapPoint: NSObject, MKAnnotation {
             locationManager.delegate = self
         }
     
-        func setup() {
+    func setup() {
 
-            //punten op kaart aanmaken
-            let mp1 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133640, 2.669839), title: "Mainstage")
-            let mp2 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.132442, 2.669345), title: "The Seahoarse Stage")
-            let mp3 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133128, 2.671019), title: "Flied Shlimp Food & Other Fish")
-            let mp4 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133048, 2.670976), title: "Fisherman's Fiend Booze & Barfs")
-            let mp5 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133115, 2.671341), title: "Restrooms Number 1")
-            let mp6 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.132859, 2.668691), title: "Restrooms Number 2")
-            let mp7 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133014, 2.671545), title: "Fisht Aid EHBO Stand")
-            let mp8 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.132785, 2.671319), title: "Seagate")
-            //punt effectief toevoegen
-            mapView.addAnnotation(mp1)
-            mapView.addAnnotation(mp2)
-            mapView.addAnnotation(mp3)
-            mapView.addAnnotation(mp4)
-            mapView.addAnnotation(mp5)
-            mapView.addAnnotation(mp6)
-            mapView.addAnnotation(mp7)
-            mapView.addAnnotation(mp8)
+        //punten op kaart aanmaken
+        let mp1 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133640, 2.669839), title: "Mainstage", pinImage: #imageLiteral(resourceName: "Annotation-pic.png") )
+        let mp2 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.132442, 2.669345), title: "The Seahoarse Stage", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        let mp3 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133128, 2.671019), title: "Flied Shlimp Food & Other Fish", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        let mp4 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133048, 2.670976), title: "Fisherman's Fiend Booze & Barfs", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        let mp5 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133115, 2.671341), title: "Restrooms Number 1", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        let mp6 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.132859, 2.668691), title: "Restrooms Number 2", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        let mp7 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.133014, 2.671545), title: "Fisht Aid EHBO Stand", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        let mp8 = MapPoint.init(coordinate: CLLocationCoordinate2DMake(51.132785, 2.671319), title: "Seagate", pinImage: #imageLiteral(resourceName: "Annotation-pic.png"))
+        //punt effectief toevoegen
+        mapView.addAnnotation(mp1)
+        mapView.addAnnotation(mp2)
+        mapView.addAnnotation(mp3)
+        mapView.addAnnotation(mp4)
+        mapView.addAnnotation(mp5)
+        mapView.addAnnotation(mp6)
+        mapView.addAnnotation(mp7)
+        mapView.addAnnotation(mp8)
             
-            
-            //zichtbaar deel op kaart bepalen
-            let region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(51.133640, 2.669839), MKCoordinateSpanMake(0.0025, 0.0025))
-            mapView.region = region
-            
-            mapView.mapType = .satellite
- 
-
+        //zichtbaar deel op kaart bepalen
+        let region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(51.133640, 2.669839), MKCoordinateSpanMake(0.0035, 0.0035))
+        mapView.region = region
+    
+        mapView.mapType = .satellite
     }
 
+    //pin personaliseren?
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+        if !(annotation is MKPointAnnotation) {
+            return nil
+        }
+        
+        let annotationIdentifier = "AnnotationIdentifier"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView!.canShowCallout = true
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        let pinImage = UIImage(named: "Annotation-pic.png")
+        annotationView!.image = pinImage
+        
+        return annotationView
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
